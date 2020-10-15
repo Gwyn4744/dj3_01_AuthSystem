82dfe09a1b4775f86adb29d42175ade4bc2e7178
Dominik Bednarski <dominikbednarski@wp.pl>
2020-10-15 14:46:20 +0200

    Created the signing up logic v0.0.4

--- auth_system/views.py
+++ auth_system/views.py
@@ -1,13 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib import auth
+
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'auth_system/home.html')
+    context = {}
+    if request.user.is_authenticated:
+        # Do something for authenticated users.
+        context['userStatus'] = 'zalogowany'
+    else:
+        # Do something for anonymous users.
+        context['userStatus'] = 'niezalogowany'
+    return render(request, 'auth_system/home.html', context)
 
 def signup_page(request):
-    return render(request, 'auth_system/signup.html')
+    context = {}
+    if request.method == 'POST':
+        # Request for sign up
+        # Check if user is available
+        try:
+            user = User.objects.get(username=request.POST['username'])
+            context['error'] = 'Podana nazwa użytkownika już istnieje! Proszę podać inną nazwę użytkownika.'
+            return render(request, 'auth_system/signup.html', context)
+        except User.DoesNotExist:
+            # Check if the password1 is equal to the password2
+            if request.POST['password1'] != request.POST['password2']:
+                context['error'] = 'Podane hasła nie są takie same! Proszę wprowadzić identyczne hasła.'
+                return render(request, 'auth_system/signup.html', context)
+            else:
+                # Create new user
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                # Automatic login after signing up
+                auth.login(request, user)
+                # Go to home page
+                return redirect('home')
+    else:
+        return render(request, 'auth_system/signup.html', context)
 
 def login_page(request):
     return render(request, 'auth_system/login.html')
