7768a85728008443ed078789fe756547eec6c95a
Dominik Bednarski <dominikbednarski@wp.pl>
2020-10-15 10:09:28 +0200

    Base sketch for paths, views and templates for signup, login, logout v0.0.3

--- auth_system/views.py
+++ auth_system/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'auth_system/home.html')
+
+def signup_page(request):
+    return render(request, 'auth_system/signup.html')
+
+def login_page(request):
+    return render(request, 'auth_system/login.html')
+
+def logout_page(request):
+    return render(request, 'auth_system/logout.html')
--- django_auth_system/urls.py
+++ django_auth_system/urls.py
@@ -20,4 +20,7 @@ from auth_system import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('signup', views.signup_page, name='signup_page'),
+    path('login', views.login_page, name='login_page'),
+    path('logout', views.logout_page, name='logout_page'),
 ]
